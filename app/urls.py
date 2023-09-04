610b9606f8a57112d7ef3f39a4374162b6b6fbcc
Pham Ngoc Kien <kienpndev@gmail.com>
2023-09-04 16:25:26 +0700

    Fix giao dien lan 2

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+
 ]
