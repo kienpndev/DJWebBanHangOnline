8d7976c5ba30d21c8550356fdc34720f4a515396
Pham Ngoc Kien <kienpndev@gmail.com>
2023-09-05 10:46:09 +0700

    Fix giao dien lan 3

--- app/models.py
+++ app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    # null = True la khi khong truyen du lieu vao thi du lieu trong database = null
+    # blank, truong co duoc phep de trong khong , false khong , true la co, khong ghi gi mac dinh la true
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=False)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+    def __str__(self) :
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+
+    def __str__(self) :
+        return self.name
+    
+class Oder(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_oder = models.DateTimeField(auto_now_add=True, )
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self) :
+        return str(self.id)
+    
+class OderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    oder = models.ForeignKey(Oder, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    oder = models.ForeignKey(Oder, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    mobile = models.CharField(max_length=10, null=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return str(self.address)
+
+    
+    
